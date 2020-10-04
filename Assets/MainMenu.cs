5f13a4e944ed6728b2ea856393a4f44297039c94
mikulangots <34790922+mikulangots@users.noreply.github.com>
2020-10-04 20:37:44 +1100

    Laast Update
    Music Toggle Button

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -7,11 +7,26 @@ using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
     public GameObject Panel;
+    public Button anyBtn, playBtn;
+    public AudioSource selectSound, startSound;
 
     public void StartGame()
     {
-
+        
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void PlaySound()
+    {
+        if (anyBtn)
+        {
+            selectSound.Play();
+        }
+        else if (playBtn)
+        {
+            startSound.Play();
+        }
+        
     }
 
     public void OpenPanel()
